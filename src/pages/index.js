0db120d1ea135244953b1d3675528422662793e0
Aileen Cross <arcross28@gmail.com>
2025-07-16 09:35:53 -0400

    favicon/page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,31 @@
-import Link from 'next/link';
+import Link from "next/link";
+import Head from "next/head";
 
 export default function Home() {
   const data = [
-    { name: 'Fridge', href: '/fridge' },
-    { name: 'Freezer', href: '/freezer' },
-    { name: 'Pantry', href: '/pantry' },
-    { name: 'Storage Room', href: '/storage-room' },
-    { name: 'Medicine', href: '/medicine' },
+    { name: "Fridge", href: "/fridge" },
+    { name: "Freezer", href: "/freezer" },
+    { name: "Pantry", href: "/pantry" },
+    { name: "Storage Room", href: "/storage-room" },
+    { name: "Medicine", href: "/medicine" },
   ];
 
   return (
-    <div className="container">
-      <h1>Inventory</h1>
-      <div className="card-list">
-        {data.map(item => (
-          <Link key={item.name} href={item.href} className="card">
-            {item.name} →
-          </Link>
-        ))}
+    <>
+      <Head>
+        <title>Home Inventory</title>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      </Head>
+      <div className="container">
+        <h1>Inventory</h1>
+        <div className="card-list">
+          {data.map((item) => (
+            <Link key={item.name} href={item.href} className="card">
+              {item.name} →
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
