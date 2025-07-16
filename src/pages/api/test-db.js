32e75121772897023ef3cb54cb8792b0a6630696
Aileen Cross <arcross28@gmail.com>
2025-07-16 11:02:07 -0400

    Update test-db.js

--- src/pages/api/test-db.js
+++ src/pages/api/test-db.js
@@ -2,7 +2,7 @@ import prisma from "@/lib/prisma";
 
 export default async function handler(req, res) {
   try {
-    const now = await prisma.$queryRaw`SELECT NOW()`;
+    const now = await prisma.$executeRaw`SELECT NOW()`;
     res.status(200).json({ time: now });
   } catch (err) {
     res.status(500).json({ error: err.message });
