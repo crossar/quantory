876b3a890860892bf2d0d96286f3f17a334b0e85
Aileen Cross <arcross28@gmail.com>
2025-07-31 13:14:08 -0400

    set up to install on mobile

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
+import nextPwa from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    esmExternals: false,
+  },
+
+  ...nextPwa({
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+  }),
 };
 
 export default nextConfig;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#4CAF50" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
